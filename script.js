741cde2ef0b9123e5b5d3ce8e8766256b492fb5f
vivaan sharma <you@example.com>
2026-02-14 09:41:49 +0530

    update the send enquiry and database

--- script.js
+++ script.js
@@ -22,3 +22,29 @@ openBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   enquiryForm.style.display = "none";
 });
+function validateForm() {
+
+  let name = document.getElementById("name").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let mobile = document.getElementById("mobile").value.trim();
+
+  let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  let mobilePattern = /^[0-9]{10}$/;
+
+  if (name.length < 3) {
+    alert("Name must be at least 3 characters");
+    return false;
+  }
+
+  if (!emailPattern.test(email)) {
+    alert("Enter valid email address");
+    return false;
+  }
+
+  if (!mobilePattern.test(mobile)) {
+    alert("Mobile number must be 10 digits");
+    return false;
+  }
+
+  return true;
+}
